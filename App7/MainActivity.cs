5d8679494d33468f861ec8766afe566b86f42f66
Максим Ларин <maksim.larin.2000@mail.ru>
2020-11-04 15:28:08 +0300

    e

--- App7/MainActivity.cs
+++ App7/MainActivity.cs
@@ -11,7 +11,7 @@ using Android.Views;
 
 namespace App7
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    [Activity(Label = "Двач", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity, NavigationView.IOnNavigationItemSelectedListener
     {
         protected override void OnCreate(Bundle savedInstanceState)
